db66f44500e55312495be34a8a4c594c40053bf5
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-08-09 08:01:48 -0600

    feat(m2): configurar relación segura imagen-variante [ChatGPT]

--- backend/src/Infrastructure/Persistence/Configurations/ProductoImagenConfiguration.cs
+++ backend/src/Infrastructure/Persistence/Configurations/ProductoImagenConfiguration.cs
@@ -9,12 +9,25 @@ public class ProductoImagenConfiguration : IEntityTypeConfiguration<ProductoImag
     public void Configure(EntityTypeBuilder<ProductoImagen> builder)
     {
         builder.ToTable("ProductoImagenes");
-        builder.Property(i => i.Url).IsRequired().HasMaxLength(500);
-        builder.Property(i => i.PublicId).IsRequired().HasMaxLength(300);
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Url).IsRequired().HasMaxLength(1000);
+        builder.Property(i => i.PublicId).IsRequired().HasMaxLength(500);
+        builder.Property(i => i.CreadoPorNombreUsuario).HasMaxLength(100);
 
         builder.HasOne(i => i.Producto)
             .WithMany(p => p.Imagenes)
             .HasForeignKey(i => i.ProductoId)
             .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(i => i.ProductoVariante)
+            .WithMany(v => v.Imagenes)
+            .HasForeignKey(i => i.ProductoVarianteId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(i => new { i.ProductoId, i.ProductoVarianteId, i.Orden })
+            .HasDatabaseName("IX_ProductoImagenes_Producto_Variante_Orden");
+        builder.HasIndex(i => new { i.ProductoVarianteId, i.EsPrincipal })
+            .HasDatabaseName("IX_ProductoImagenes_Variante_Principal");
     }
 }
