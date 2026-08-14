339b038054ca1489ef843e4469234de84ffa81f3
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-08-13 22:35:08 -0600

    feat(N0.7.D): contrato paginado de ajustes de inventario [VAEP]

--- backend/src/Application/Interfaces/IAjusteInventarioRepository.cs
+++ backend/src/Application/Interfaces/IAjusteInventarioRepository.cs
@@ -1,3 +1,4 @@
+using InventoryApp.Application.DTOs;
 using InventoryApp.Domain.Entities;
 
 namespace InventoryApp.Application.Interfaces;
@@ -5,6 +6,7 @@ namespace InventoryApp.Application.Interfaces;
 public interface IAjusteInventarioRepository
 {
     Task<List<AjusteInventario>> GetAllAsync();
+    Task<(List<AjusteInventario> Items, int TotalCount)> GetPagedAsync(AjusteInventarioFiltroDto filtro);
     Task<AjusteInventario?> GetByIdAsync(int id);
     Task<AjusteInventario?> GetByIdForUpdateAsync(int id);
     Task AddAsync(AjusteInventario ajuste);
