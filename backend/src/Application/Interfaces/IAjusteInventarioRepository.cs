017e9b679de54a8e052b3aeeae9e3f20b1a40203
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-08-13 22:17:19 -0600

    feat(N0.7.D): definir repositorio de ajustes de inventario [VAEP]

---
+++ backend/src/Application/Interfaces/IAjusteInventarioRepository.cs
@@ -0,0 +1,13 @@
+using InventoryApp.Domain.Entities;
+
+namespace InventoryApp.Application.Interfaces;
+
+public interface IAjusteInventarioRepository
+{
+    Task<List<AjusteInventario>> GetAllAsync();
+    Task<AjusteInventario?> GetByIdAsync(int id);
+    Task<AjusteInventario?> GetByIdForUpdateAsync(int id);
+    Task AddAsync(AjusteInventario ajuste);
+    void Update(AjusteInventario ajuste);
+    Task SaveChangesAsync();
+}
