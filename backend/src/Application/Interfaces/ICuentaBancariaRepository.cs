03d08eebf20dc3bd867cb08e718ad316778e012d
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-09-01 21:44:01 -0600

    feat(bancos): page cuenta repository contract

--- backend/src/Application/Interfaces/ICuentaBancariaRepository.cs
+++ backend/src/Application/Interfaces/ICuentaBancariaRepository.cs
@@ -1,11 +1,12 @@
 using InventoryApp.Domain.Entities.Bancos;
+using InventoryApp.Application.Bancos;
 
 namespace InventoryApp.Application.Interfaces;
 
 public interface ICuentaBancariaRepository
 {
     Task<CuentaBancaria?> GetByIdAsync(int id);
-    Task<List<CuentaBancaria>> GetAllAsync();
+    Task<CuentaBancariaPage<CuentaBancaria>> GetAllAsync(CuentaBancariaQueryFilter filter);
     Task<List<CuentaBancaria>> GetActivasAsync();
     Task AddAsync(CuentaBancaria cuenta);
     void Update(CuentaBancaria cuenta);
