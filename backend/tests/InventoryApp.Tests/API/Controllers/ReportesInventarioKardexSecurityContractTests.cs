46805c24adfd3163baa17590bb1bc7ccee148c44
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-09-09 14:06:08 -0600

    fix(N5.2.F): repair kardex security test imports

--- backend/tests/InventoryApp.Tests/API/Controllers/ReportesInventarioKardexSecurityContractTests.cs
+++ backend/tests/InventoryApp.Tests/API/Controllers/ReportesInventarioKardexSecurityContractTests.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using InventoryApp.API.Controllers;
 using InventoryApp.API.Filters;
+using InventoryApp.Application.Common;
+using InventoryApp.Application.DTOs;
 using InventoryApp.Application.Interfaces;
 using InventoryApp.Domain.Enums;
 using Microsoft.AspNetCore.Authorization;
