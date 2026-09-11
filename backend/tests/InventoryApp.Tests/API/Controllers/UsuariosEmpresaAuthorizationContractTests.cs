d8c8a238f3a20aa648f53466aca0d15fa6f873a7
jmejia31 <93171442+jmejia31@users.noreply.github.com>
2026-09-11 12:59:27 -0600

    test(n6.4.f): prove UsuarioEmpresa authorization contracts

---
+++ backend/tests/InventoryApp.Tests/API/Controllers/UsuariosEmpresaAuthorizationContractTests.cs
@@ -0,0 +1,102 @@
+using System.Reflection;
+using InventoryApp.API.Controllers;
+using InventoryApp.API.Filters;
+using InventoryApp.Application.DTOs;
+using InventoryApp.Application.Interfaces;
+using InventoryApp.Domain.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace InventoryApp.Tests.API.Controllers;
+
+public class UsuariosEmpresaAuthorizationContractTests
+{
+    private readonly Type _controllerType = typeof(UsuariosController);
+
+    [Fact]
+    public void Controller_DebeRequerirAutenticacion_Y_RutaUsuarios()
+    {
+        Assert.NotNull(_controllerType.GetCustomAttribute<AuthorizeAttribute>());
+        var route = _controllerType.GetCustomAttribute<RouteAttribute>();
+        Assert.NotNull(route);
+        Assert.Equal("usuarios", route.Template);
+    }
+
+    [Theory]
+    [InlineData(nameof(UsuariosController.GetEmpresas), typeof(HttpGetAttribute), "{id:int}/empresas", AccionPermiso.Ver)]
+    [InlineData(nameof(UsuariosController.AsignarEmpresa), typeof(HttpPostAttribute), "{id:int}/empresas", AccionPermiso.AsignarRol)]
+    [InlineData(nameof(UsuariosController.CambiarRolEmpresa), typeof(HttpPutAttribute), "{id:int}/empresas/{empresaId:int}/rol", AccionPermiso.AsignarRol)]
+    public void Endpoints_Empresa_DebenExigirPermisoUsuarios(
+        string methodName,
+        Type expectedVerb,
+        string expectedTemplate,
+        AccionPermiso expectedAccion)
+    {
+        var method = _controllerType.GetMethod(methodName);
+        Assert.NotNull(method);
+
+        var verb = method.GetCustomAttributes().Single(attribute => expectedVerb.IsInstanceOfType(attribute));
+        var template = verb switch
+        {
+            HttpGetAttribute get => get.Template,
+            HttpPostAttribute post => post.Template,
+            HttpPutAttribute put => put.Template,
+            _ => null
+        };
+        Assert.Equal(expectedTemplate, template);
+
+        var permiso = method.GetCustomAttribute<RequierePermisoAttribute>();
+        Assert.NotNull(permiso);
+
+        var moduloField = typeof(RequierePermisoAttribute).GetField("_modulo", BindingFlags.NonPublic | BindingFlags.Instance);
+        var accionField = typeof(RequierePermisoAttribute).GetField("_accion", BindingFlags.NonPublic | BindingFlags.Instance);
+        Assert.NotNull(moduloField);
+        Assert.NotNull(accionField);
+        Assert.Equal(ModuloSistema.Usuarios, (ModuloSistema?)moduloField.GetValue(permiso));
+        Assert.Equal(expectedAccion, (AccionPermiso?)accionField.GetValue(permiso));
+    }
+
+    [Fact]
+    public void CambiarEstadoEmpresa_DebeExigirPermisoActivar_CuandoActivaEsTrue()
+    {
+        var usuarioService = new Mock<IUsuarioService>();
+        var permisoService = new Mock<IPermisoService>();
+        var currentUser = new Mock<ICurrentUserService>();
+        usuarioService
+            .Setup(service => service.CambiarEstadoEmpresaAsync(7, 11, true))
+            .ReturnsAsync((UsuarioEmpresaDto)null!);
+
+        var controller = new UsuariosController(usuarioService.Object, permisoService.Object, currentUser.Object);
+
+        var result = controller.CambiarEstadoEmpresa(7, 11, new UpdateUsuarioEmpresaEstadoDto { Activa = true });
+        result.GetAwaiter().GetResult();
+
+        permisoService.Verify(
+            service => service.VerificarPermisoAsync(ModuloSistema.Usuarios, AccionPermiso.Activar),
+            Times.Once);
+        usuarioService.Verify(service => service.CambiarEstadoEmpresaAsync(7, 11, true), Times.Once);
+    }
+
+    [Fact]
+    public void CambiarEstadoEmpresa_DebeExigirPermisoDesactivar_CuandoActivaEsFalse()
+    {
+        var usuarioService = new Mock<IUsuarioService>();
+        var permisoService = new Mock<IPermisoService>();
+        var currentUser = new Mock<ICurrentUserService>();
+        usuarioService
+            .Setup(service => service.CambiarEstadoEmpresaAsync(7, 11, false))
+            .ReturnsAsync((UsuarioEmpresaDto)null!);
+
+        var controller = new UsuariosController(usuarioService.Object, permisoService.Object, currentUser.Object);
+
+        var result = controller.CambiarEstadoEmpresa(7, 11, new UpdateUsuarioEmpresaEstadoDto { Activa = false });
+        result.GetAwaiter().GetResult();
+
+        permisoService.Verify(
+            service => service.VerificarPermisoAsync(ModuloSistema.Usuarios, AccionPermiso.Desactivar),
+            Times.Once);
+        usuarioService.Verify(service => service.CambiarEstadoEmpresaAsync(7, 11, false), Times.Once);
+    }
+}
